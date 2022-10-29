5edff61b5c45a62b4381c5dfddfd66c4052735bb
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2022-10-30 04:35:24 +0500

    register login newpassword UI finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import {BrowserRouter} from 'react-router-dom'
+import SignedUser from './SignedUser.jsx'
+import UnsignedUser from './UnsignedUser.jsx'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isSigned, setIsSigned] = useState(true)
+
+  return isSigned ? (
+    <BrowserRouter>
+      <SignedUser />
+    </BrowserRouter>
+  ) : (
+    <UnsignedUser />
+  )
 }
 
 export default App;
